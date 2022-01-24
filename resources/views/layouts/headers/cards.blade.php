22b80673a90149ccb4184277896fd638d8a04336
Mauro Fatu <maurofatu@hotmail.com>
2022-01-24 08:24:54 -0500

    Variable Session

--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -9,7 +9,7 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Capitanes</h5>
-                                    <span class="h2 font-weight-bold mb-0">350,8978</span>
+                                    <span class="h2 font-weight-bold mb-0"> {{ Session::get('totalcapi'); }} </span>
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
@@ -30,7 +30,8 @@
                             <div class="row">
                                 <div class="col">
                                     <h5 class="card-title text-uppercase text-muted mb-0">Jugadores</h5>
-                                    <span class="h2 font-weight-bold mb-0">2,356</span>
+                                    <span class="h2 font-weight-bold mb-0"> {{ Session::get('totaljuga'); }} </span>
+                                    {{-- <span class="h2 font-weight-bold mb-0">1.857</span> --}}
                                 </div>
                                 <div class="col-auto">
                                     <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
