43256189bca76c9c5440b0587156a2a163d02306
David Diaz <hddiazv@gmail.com>
2022-01-25 08:52:21 -0500

    change images of blue.png to galaxiacolor.png

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -71,7 +71,7 @@
     </button>
     <!-- Brand -->
     <a class="navbar-brand pt-0" href="{{ route('home') }}">
-        <img src="{{ asset('argon') }}/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        <img src="{{ asset('argon') }}/img/brand/galaxiacolor.png" class="navbar-brand-img" alt="...">
     </a>
     <!-- User -->
     <ul class="nav align-items-center d-md-none">
@@ -119,7 +119,7 @@
             <div class="row">
                 <div class="col-6 collapse-brand">
                     <a  href="{{ route('home') }}">
-                        <img src="{{ asset('argon') }}/img/brand/blue.png">
+                        <img src="{{ asset('argon') }}/img/brand/galaxiacolor.png">
                     </a>
                 </div>
                 <div class="col-6 collapse-close">
